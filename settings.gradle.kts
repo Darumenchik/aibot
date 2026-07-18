8beeff163b4e3e2f177d1c1d6ece1d2c0b79dcd6
darumenchik <dudutuka2014@gmail.com>
2026-07-18 12:00:33 +0500

    Fix: use voyager 1.0.0 and add jetbrains space repo for markdown

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = java.net.URI("https://maven.pkg.jetbrains.space/public/p/compose/compose") }
     }
 }
 
