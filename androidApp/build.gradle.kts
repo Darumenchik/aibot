d5503ccefd103e7f8db275b239d3307299432a6c
darumenchik <dudutuka2014@gmail.com>
2026-07-18 12:19:08 +0500

    Fix: completely reset androidApp gradle build file syntax

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,30 +1,8 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.androidApplication)
-    id("org.jetbrains.kotlin.android")
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.kotlinSerialization)
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = JvmTarget.JVM_11
-    }
-}
-
-dependencies {
-    implementation(projects.shared)
-
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.core.ktx)
-
-    // Koin Android
-    implementation(libs.koin.android)
-
-    implementation(libs.compose.uiToolingPreview)
-    debugImplementation(libs.compose.uiTooling)
+    alias(libs.plugins.kotlinAndroid)
 }
 
 android {
@@ -38,11 +16,6 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
@@ -52,10 +25,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    
-    // Enable edge-to-edge
-    namespace = "com.chiper.kz"
+}
 
 dependencies {
+    implementation(project(":shared"))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.material3)
     implementation("com.google.android.material:material:1.12.0")
 }
